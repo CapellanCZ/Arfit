186f75752b9ff878ac26c69b6bd64ab80372e705
yunac <yunac@Yuna>
2024-06-11 01:04:49 +0800

    Bulldogs Exchange Products - [XAAMP] [CRUD-SYSTEM]

---
+++ src/com/arfit/components/bggradient.java
@@ -0,0 +1,45 @@
+
+package com.arfit.components;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+
+public class bggradient extends javax.swing.JPanel {
+
+    public bggradient() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 233, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    protected void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); 
+        GradientPaint gp = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0,  getHeight(), Color.decode("#000046"));
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintChildren(g); //To change body of generated methods, choose Tools | Templates.
+    }
+}
